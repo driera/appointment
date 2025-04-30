550ece9b7c6f5b933ff6107cf5585eaeca3a8168
Dani Riera <daniel.riera@qatium.com>
2025-04-30 19:59:55 +0200

    Make Appointment return customer name

--- src/test/Appointment.test.js
+++ src/test/Appointment.test.js
@@ -1,5 +1,20 @@
+import React, {act} from "react";
+import ReactDOM from "react-dom/client";
+import { Appointment} from "./Appointment"
+
 describe("Appointment", () => {
   it("renders the customer first name", () => {
+    const customer = { firstName: "Ashley"};
+    const component = (
+      <Appointment customer={customer} />
+    );
+    const container = document.createElement("div");
+    document.body.appendChild(container);
+
+    act(() => {
+      ReactDOM.createRoot(container).render(component);
+    });
+
     expect(document.body.textContent).toContain("Ashley")
   })
 });
