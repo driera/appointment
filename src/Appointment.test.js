e11c47c9573ef621c7ed31249e9be2c7890cdd8b
Dani Riera <daniel.riera@qatium.com>
2025-04-30 20:31:14 +0200

    Extract act to shared method

--- src/Appointment.test.js
+++ src/Appointment.test.js
@@ -9,28 +9,22 @@ describe("Appointment", () => {
     document.body.replaceChildren(container);
   });
 
+  const render = component => act(() => {
+    ReactDOM.createRoot(container).render(component);
+  });
+
   it("renders the customer first name", () => {
     const customer = { firstName: "Ashley"};
-    const component = (
-      <Appointment customer={customer} />
-    );
 
-    act(() => {
-      ReactDOM.createRoot(container).render(component);
-    });
+    render(<Appointment customer={customer} />);
 
     expect(document.body.textContent).toContain("Ashley")
   })
 
   it("renders another customer first name", () => {
     const customer = { firstName: "Jordan"};
-    const component = (
-      <Appointment customer={customer} />
-    );
 
-    act(() => {
-      ReactDOM.createRoot(container).render(component);
-    });
+    render(<Appointment customer={customer} />)
 
     expect(document.body.textContent).toContain("Jordan")
   })
